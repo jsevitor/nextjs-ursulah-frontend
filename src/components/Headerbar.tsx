a412ac779cb1006c2631d0cf24f8bb4f590e51d7
José Vitor Oliveira Reis <vitorjseo@gmail.com>
2024-07-05 20:59:13 -0300

    responsiveness implemented

--- src/components/Headerbar.tsx
+++ src/components/Headerbar.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/Headerbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
 const Headerbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <Link href={"https://grantoseguros.com/"}>
-          <Image src="/logo.svg" width={90} height={40} alt="Logo" />
+          <picture>
+            <source media="(max-width: 768px)" srcSet="logo-g.png" />
+            <Image
+              src="/logo.svg"
+              width={90}
+              height={40}
+              alt="Logo"
+              className={styles.logo_img}
+            />
+          </picture>
         </Link>
       </div>
       <nav className={styles.nav_menu}>
-        <ul className={styles.menu_items}>
+        <ul className={`${styles.menu_items} ${menuOpen ? styles.open : ""}`}>
           <li>
             <Link
               href={"https://jsevitor.github.io/desafio-granto-landing-page/"}
@@ -29,6 +44,15 @@ const Headerbar = () => {
           <Link href={"/login"}>
             <button>Fazer Login</button>
           </Link>
+        </div>
+        <div className={styles.menu_icon}>
+          <Image
+            src="/burger-menu.svg"
+            width={24}
+            height={24}
+            alt="Menu icon"
+            onClick={toggleMenu}
+          />
         </div>
       </nav>
     </header>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,6 @@ import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap"
-          rel="stylesheet"
-        />
-        <title>Granto US</title>
-      </Head>
       <Component {...pageProps} />
     </>
   );
